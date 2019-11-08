b1dbf0ad3d5718a3a23d42c6be9f4a56a7d08e0f
sachin156 <sachinvadlakonda999@gmail.com>
2019-11-08 11:03:42 +0530

    intent

--- nlpbot/intentclassification.py
+++ nlpbot/intentclassification.py
@@ -21,22 +21,3 @@ def getintent(text):
 
 
 
-# from rasa_nlu.training_data  import load_data
-# from rasa_nlu.config import RasaNLUModelConfig
-# from rasa_nlu.model import Trainer
-# from rasa_nlu import config
-# from rasa_nlu.model import Metadata, Interpreter
-# import sys
-
-# train_data = load_data('rasa_dataset.json')
-# trainer = Trainer(config.load("config_spacy.yaml"))
-# trainer.train(train_data)
-# model_directory = trainer.persist('./projects/')
-# interpreter = Interpreter.load(model_directory)
-
-
-
-# def getintent(text):
-# 	intentname=interpreter.parse(text)
-# 	return intentname['intent']['name']
-# print(getintent("Book an appointment"))
